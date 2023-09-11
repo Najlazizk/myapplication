04fc63f6c432126846e4f8f2604c74280e86b27f
Najlazizk <dosarynja@gmail.com>
2023-09-11 11:14:47 +0300

    lab2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.TextView;
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button send_button;
     EditText send_text;
-    TextView receiver_msg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         send_button= findViewById(R.id.send_button_id);
         send_text = findViewById(R.id.send_text_id);
-        receiver_msg = (TextView) findViewById(R.id.received_value_id);
+
 
 
         send_button.setOnClickListener(v -> {
 
             String str = send_text.getText().toString();
 
-            receiver_msg.setText("hello"+str);
+            Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+            intent.putExtra("message_key",str);
+            startActivity(intent);
         });
     }
 }
